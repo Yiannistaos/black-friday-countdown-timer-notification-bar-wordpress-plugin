46a42a62217814da63a706330766da84631cc0b9
Yiannistaos <yiannistaos@gmail.com>
2020-11-25 15:41:14 +0200

    [JS improvement] IIFEs support in JS [Thank you, Gerasimos Tsiamalos]
    http://adripofjavascript.com/blog/drips/an-introduction-to-iffes-immediately-invoked-function-expressions.html

--- js/script.js
+++ js/script.js
@@ -1,55 +1,62 @@
-// Set the date we're counting down to
-var countDownDate = new Date("2020-11-28").getTime();
 
-// Get today's date and time
-var now = new Date().getTime();
+(function () {
 
-// Find the distance between now and the count down date
-var distance = countDownDate - now;
-// If the count down is finished, write some text
-if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("blackfriday-html-toolbar-content").innerHTML = "The Black Friday 2020 has been expired. Thank you!";
-}
-else
-{
-    // Update the count down every 1 second
-    var x = setInterval(function () {
+    // Set the date we're counting down to
+    var countDownDate = new Date("2020-11-28").getTime();
 
-        // Get today's date and time
-        var now = new Date().getTime();
+    console.log("countDownDate1:" + countDownDate);
 
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+    // Get today's date and time
+    var now = new Date().getTime();
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("blackfriday-html-toolbar-content").innerHTML = "The Black Friday 2020 has been expired. Thank you!";
+    }
+    else
+    {
+        // Update the count down every 1 second
+        var x = setInterval(function () {
 
-        days = ('0' + days).slice(-2);
-        hours = ('0' + hours).slice(-2);
-        minutes = ('0' + minutes).slice(-2);
-        seconds = ('0' + seconds).slice(-2);
-    
-        // Display the result in the element with id="blackfriday-countdown-timer"
-        var html = '<div class="blackfriday-countdown-timer">';
-        html += '<div class="blackfriday-countdown-days">' + days + '<div class="txt">Days</div></div>';
-        html += '<div class="blackfriday-countdown-days">' + hours + '<div class="txt">Hours</div></div>';
-        html += '<div class="blackfriday-countdown-days">' + minutes + '<div class="txt">Minutes</div></div>';
-        html += '<div class="blackfriday-countdown-days">' + seconds + '<div class="txt">Seconds</div></div>';
-        html += '</div>';
+            // Get today's date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            days = ('0' + days).slice(-2);
+            hours = ('0' + hours).slice(-2);
+            minutes = ('0' + minutes).slice(-2);
+            seconds = ('0' + seconds).slice(-2);
+        
+            // Display the result in the element with id="blackfriday-countdown-timer"
+            var html = '<div class="blackfriday-countdown-timer">';
+            html += '<div class="blackfriday-countdown-days">' + days + '<div class="txt">Days</div></div>';
+            html += '<div class="blackfriday-countdown-days">' + hours + '<div class="txt">Hours</div></div>';
+            html += '<div class="blackfriday-countdown-days">' + minutes + '<div class="txt">Minutes</div></div>';
+            html += '<div class="blackfriday-countdown-days">' + seconds + '<div class="txt">Seconds</div></div>';
+            html += '</div>';
 
 
-        html += "</div>";
-        document.getElementById("blackfriday-countdown-timer").innerHTML = html;
+            html += "</div>";
+            document.getElementById("blackfriday-countdown-timer").innerHTML = html;
 
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("blackfriday-html-toolbar-content").innerHTML = "The Black Friday 2020 has been expired. Thank you! :)";
-        }
-    }, 1000);
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("blackfriday-html-toolbar-content").innerHTML = "The Black Friday 2020 has been expired. Thank you! :)";
+            }
+        }, 1000);
 
-}
+    }
+
+})();
